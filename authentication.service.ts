882ed79d70a80c6dbda80acbc361c34e21440276
AmitDasRup <47097057+AmitDasRup@users.noreply.github.com>
2020-01-01 07:08:41 -0600

    Add files via upload

---
+++ authentication.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from "@angular/fire/auth";
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthenticationService {
+  userData: Observable<firebase.User>;
+
+  constructor(private angularFireAuth: AngularFireAuth) {
+    this.userData = angularFireAuth.authState;
+
+    this.userData.subscribe(
+    user => this.SaveIdTokenInLocalStorage(user.getIdToken()),
+    err => console.error('userData got an error: ' + err),
+    () => console.log('Observer got a complete notification')
+);
+  }
+
+  SaveIdTokenInLocalStorage(idToken){
+    idToken.then( value => {
+      localStorage.setItem('idtoken', value);
+    });
+  }
+
+  /* Sign up */
+  SignUp(email: string, password: string) {
+    this.angularFireAuth
+      .auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log('Successfully signed up!', res);
+      })
+      .catch(error => {
+        console.log('Something is wrong:', error.message);
+      });    
+  }
+
+  /* Sign in */
+  SignIn(email: string, password: string) {
+    this.angularFireAuth
+      .auth
+      .signInWithEmailAndPassword(email, password)
+      .then(res => {
+        console.log('Successfully signed in!');
+      })
+      .catch(err => {
+        console.log('Something is wrong:',err.message);
+      });
+  }
+
+  /* Sign out */
+  SignOut() {
+    this.angularFireAuth
+      .auth
+      .signOut();
+  }
+  }
+
