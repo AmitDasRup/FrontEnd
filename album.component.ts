35403300730294635d2260f5fa309dc3abbfc13c
AmitDasRup <47097057+AmitDasRup@users.noreply.github.com>
2020-01-09 15:37:10 -0600

    Add files via upload

--- album.component.ts
+++ album.component.ts
@@ -9,32 +9,20 @@ import {AlbumService} from '../album.service';
 })
 export class AlbumComponent implements OnInit {
 
-  public myName="";
-
-  public album: Album[]=[{
-    id:"1",
-    name:"Album 1",
-    coverPhotoUrl:"https://www.w3.org/MarkUp/Test/xhtml-print/20050519/tests/jpeg420exif.jpg",
-  },
-  {
-  id:"2",
-    name:"Album 2",
-    coverPhotoUrl:"https://www.w3.org/MarkUp/Test/xhtml-print/20050519/tests/jpeg420exif.jpg",
-  },
-  {
-  id:"3",
-    name:"Album 3",
-    coverPhotoUrl:"https://www.w3.org/MarkUp/Test/xhtml-print/20050519/tests/jpeg420exif.jpg",
-  },
-
-  ];
-
+  album: Album[]=[];
+   
   constructor(
     private albumService: AlbumService,
   ) { }
 
   ngOnInit() {
-  this.myName="Amit"
+
+  this.albumService.getMyAlbum()
+  .subscribe(
+    result => this.album = <Album[]>result,
+    err => console.error("Got an error", + err),
+    () => console.log("Got a complete notification")
+  );
   }
 
 }
