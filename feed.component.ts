9c3513b5a62b04c686c49b469faa482d86341570
AmitDasRup <47097057+AmitDasRup@users.noreply.github.com>
2020-01-09 15:38:55 -0600

    Add files via upload

---
+++ feed.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Album} from '../Album';
+import {AlbumService} from '../album.service';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.css']
+})
+export class FeedComponent implements OnInit {
+
+  album: Album[]=[];
+   
+  constructor(
+    private albumService: AlbumService,
+  ) { }
+
+  ngOnInit() {
+
+  this.albumService.getAllAlbum()
+  .subscribe(
+    result => this.album = (<Album[]>result).slice(-10, -1),
+    err => console.error("Got an error", + err),
+    () => console.log("Got a complete notification")
+  );
+  }
+
+}
