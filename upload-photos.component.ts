5ed65195906a25475e010f5b6a81a51a5011e446
AmitDasRup <47097057+AmitDasRup@users.noreply.github.com>
2020-01-09 15:41:02 -0600

    Add files via upload

---
+++ upload-photos.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+//import {AlbumService} from '../album.service';
+import { PhotoService } from '../photo.service';
+
+
+@Component({
+  selector: 'app-upload-photos',
+  templateUrl: './upload-photos.component.html',
+  styleUrls: ['./upload-photos.component.css']
+})
+export class UploadPhotosComponent implements OnInit {
+
+  private albumId: String;
+  private albumTitle: String;
+
+  afuConfig = {
+    uploadAPI: {
+      url:"http://ec2-54-218-206-93.us-west-2.compute.amazonaws.com:7000/api/files/upload"
+    }
+};
+
+  constructor(
+    private route: ActivatedRoute,
+    //private albumService: AlbumService,
+    private photoService: PhotoService,
+  ) { }
+
+  ngOnInit() {
+
+    this.route.paramMap.subscribe(params => {
+    this.albumId = params.get('albumId');
+    this.albumTitle = params.get('albumTitle');
+    //this.albumService.getAlbumDetails(this.albumId);
+  });
+  }
+
+  fileUploaded(uploadEvent){
+    var uploadedFile = JSON.parse(uploadEvent.responseText);
+    console.log("Upload fileId:", uploadedFile.fileId);
+    //this.photoService.savePhoto(uploadedFile.fileId, this.albumId);
+
+    this.photoService.savePhoto(uploadedFile.fileId, this.albumId)
+    .subscribe(
+    result => console.log("Photo upload response", result),
+    err => console.error("Got an error", + err),
+    () => console.log("Got a complete notification")
+  );
+
+  }
+
+}
